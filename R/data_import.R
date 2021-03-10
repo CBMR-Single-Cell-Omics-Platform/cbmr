e7ac9bdc4d87386e429080ff3956edf66f8cb9b6
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2021-03-10 13:01:48 +0100

    initial bisulfite functions

--- R/data_import.R
+++ R/data_import.R
@@ -27,7 +27,7 @@ featurecounts_reader <- function(x, regex){
 #' @param regex optional regular expression for updating sample names. 
 #' Default option works with the fq2count pipeline. Set to NULL to disable.
 #'
-#' @importFrom data.table :=
+#' @import data.table
 #' @return data.table of gene counts and formatted gene information. FeatureCounts
 #' reports chromosome, start, end and strand for each exon. These are reformatted
 #' so start contains the start of the first exon and end the end of the last exon.
@@ -95,3 +95,76 @@ prepare_rna_dgelist <- function(counts, metadata, sample_col = "Sample ID") {
   y
 }
 
+#' Prepare a list of cov files for DMR analysis
+#'
+#' @param files character vector
+#' @param BSGenome the BSGenome object for the organism analyzed
+#'
+#' @return list of data.tables with the same names as files.
+#' @import data.table
+#' @export
+#'
+#' @examples
+prepare_cov_files <- function(files, BSGenome = NULL) {
+  if (!is.null(BSGenome)) {
+    common_chrs <- GenomeInfoDb::standardChromosomes(BSGenome)
+    
+    count_fn <- function(x){
+      cpgs <- Biostrings::matchPattern(pattern = "CG", subject = BSGenome[[x]])
+      data.table::data.table(seqnames = x, start = start(cpgs), end = end(cpgs))
+    }
+    
+    all_cpgs <- lapply(common_chrs, count_fn)
+    names(all_cpgs) <- common_chrs
+    all_cpgs <- data.table::rbindlist(all_cpgs)
+    all_cpgs[, id:=seq_len(.N)]
+    data.table::setkey(all_cpgs, seqnames, start, end)
+  } else {
+    all_cpgs <- NULL
+  }
+  
+  lapply(files, covfile_reader, cpgs = all_cpgs)
+}
+
+#' Read cov file and optionally aggregate to known CpGs
+#'
+#' @param file 
+#' @param cpgs 
+#'
+#' @return
+#' @import data.table
+#' @export
+#'
+#' @examples
+covfile_reader <- function(file, cpgs = NULL) {
+  cov_data <- data.table::fread(file, select = c(1:3,5:6))
+  data.table::setnames(cov_data, c("seqnames", "start", "end", "Me", "Un"))
+  
+  if (!is.null(cpgs)) { # Location of known CpGs is supplied
+    # Sometimes the reads will use ENSMBL convention (seqnames are e.g. 1, 2, 3)
+    # while the BSGenome uses UCSC convention (seqnames are e.g. chr1, chr2, chr3)
+    # in that case prepend chr to seqnames
+    cpgs_has_chr <- any(stringr::str_detect(cpgs$seqnames, "^chr"))
+    seq_has_chr <- any(stringr::str_detect(cov_data$seqnames, "^chr"))
+    
+    if (cpgs_has_chr & !seq_has_chr){
+      cov_data[, seqnames:=paste0("chr", seqnames)]
+    }
+    
+    setkey(cov_data, seqnames, start, end)
+    
+    ovlp <- foverlaps(cov_data, cpgs)
+    
+    seqnames <- start <- end <- Me <- Un <- NULL # Silence warnings
+    out <- ovlp[, .(seqnames = seqnames[[1]],
+             start = start[[1]],
+             end = end[[1]],
+             Me = sum(Me),
+             Un = sum(Un)), by = "id"]
+    out[, id:=NULL]
+  } else {
+    out <- cov_data
+  }
+  
+  out[]
+}
