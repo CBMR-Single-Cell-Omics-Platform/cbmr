8b2b4f164890771882609217d987065cd81e9d13
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2022-11-21 10:46:18 +0100

    Add symlog transformation

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -67,6 +67,51 @@ find_pvalue_cutoff <- function(x, cutoff = 0.05)
   optimize(diffFn, pvals[c(firstFailing - 1, firstFailing)])$minimum
 }
 
+#' Symlog transformation
+#'
+#' @param base Exponential base, default is 10
+#' @param thr Threshold for linearity, default is 1
+#' @param scale Scale factor applied to the log transformed data
+#' @description 
+#' A log scale that handles negative values and is linear around 0. Similar to
+#' the symlog from Python.
+#' Copied from: https://stackoverflow.com/questions/14613355/how-to-get-something-like-matplotlibs-symlog-scale-in-ggplot-or-lattice
+#' @return
+#' @export
+#'
+#' @examples
+symlog_trans <- function(base = 10, thr = 1, scale = 1){
+  trans <- function(x)
+    ifelse(abs(x) < thr, x, sign(x) * 
+             (thr + scale * suppressWarnings(log(sign(x) * x / thr, base))))
+  
+  inv <- function(x)
+    ifelse(abs(x) < thr, x, sign(x) * 
+             base^((sign(x) * x - thr) / scale) * thr)
+  
+  breaks <- function(x){
+    sgn <- sign(x[which.max(abs(x))])
+    if(all(abs(x) < thr))
+      pretty_breaks()(x)
+    else if(prod(x) >= 0){
+      if(min(abs(x)) < thr)
+        sgn * unique(c(pretty_breaks()(c(min(abs(x)), thr)),
+                       log_breaks(base)(c(max(abs(x)), thr))))
+      else
+        sgn * log_breaks(base)(sgn * x)
+    } else {
+      if(min(abs(x)) < thr)
+        unique(c(sgn * log_breaks()(c(max(abs(x)), thr)),
+                 pretty_breaks()(c(sgn * thr, x[which.min(abs(x))]))))
+      else
+        unique(c(-log_breaks(base)(c(thr, -x[1])),
+                 pretty_breaks()(c(-thr, thr)),
+                 log_breaks(base)(c(thr, x[2]))))
+    }
+  }
+  trans_new(paste("symlog", thr, base, scale, sep = "-"), trans, inv, breaks)
+}
+
 #' Genomic log transformation
 #' @details 
 #' Returns a transformer that nicely handles log10 scale in a genomic context.
