0dcbff76d2c3adcfd0abdaf1447b43f94b4c013d
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2022-11-30 09:51:41 +0100

    function to read a file line by line

--- R/utility.R
+++ R/utility.R
@@ -7,3 +7,38 @@
 fix_nonbreaking_space <- function(x) {
   gsub("(*UCP)\\s", " ", x, perl = TRUE)
 }
+
+#' Apply function line by line
+#'
+#' @param file path to file to be processed
+#' @param fun function to be applied to line
+#' @param ... other parameters applied to function
+#'
+#' @return
+#' @export
+#'
+#' @examples
+apply_by_line <- function(file, fun, ..., chunks = 10000) {
+  connection <- file(file, open = "r")
+  
+  if (tools::file_ext(file) == "gz") {
+    connection <- gzcon(connection)
+  }
+  
+  out <- vector(mode = "list", length = chunks)
+  n <- 1
+  
+  while(TRUE) {
+    line <- readLines(con = connection, n = 1)
+    if (length(line) == 0) {
+      break
+    }
+    if (n %% chunks == 0) {
+      out <- c(out, vector(mode = "list", length = chunks))
+    }
+    out[[n]] <- fun(line, ...)
+    n <- n + 1
+  }
+  close(connection)
+  out[lengths(out) != 0]
+}
